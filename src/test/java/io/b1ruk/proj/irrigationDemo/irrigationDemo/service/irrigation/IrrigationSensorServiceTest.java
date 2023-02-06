0cda5009919c66fd865f90e3edbfbc8c4d9906a6
b1ruk <biruk3870@gmail.com>
2023-02-06 06:17:28 +0300

    springBoot test config for IrrigationSensorServiceTest

--- src/test/java/io/b1ruk/proj/irrigationDemo/irrigationDemo/service/irrigation/IrrigationSensorServiceTest.java
+++ src/test/java/io/b1ruk/proj/irrigationDemo/irrigationDemo/service/irrigation/IrrigationSensorServiceTest.java
@@ -1,12 +1,17 @@
 package io.b1ruk.proj.irrigationDemo.irrigationDemo.service.irrigation;
 
 import io.b1ruk.proj.irrigationDemo.irrigationDemo.exception.IrrigationInitException;
+import io.b1ruk.proj.irrigationDemo.irrigationDemo.exception.SensorNotAvailableException;
 import io.b1ruk.proj.irrigationDemo.irrigationDemo.model.Land;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.retry.annotation.EnableRetry;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -15,9 +20,10 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.doThrow;
 
-class IrrigationMockImplementationServiceTest {
+@EnableRetry
+@SpringBootTest(classes = {IrrigationSensorService.class,IrrigationMockImplementationService.class})
+class IrrigationSensorServiceTest {
 
-    @Mock
     private IrrigationSensorService irrigationSensorService;
 
 
@@ -35,17 +41,35 @@ class IrrigationMockImplementationServiceTest {
     }
 
     @Test
-    @DisplayName("Should initialize land for irrigation")
-    void shouldInitLandForIrrigation(){
+    @DisplayName("Should through an exception initialize land for irrigation")
+    void shouldThroughExceptionInitLandForIrrigation(){
+
         assertThatThrownBy(()->irrigationSensorService.initLandForIrrigation(land))
                 .isInstanceOf(IrrigationInitException.class);
     }
 
     @Test
     @DisplayName("Should timeout when initializing land for irrigation")
-    void shouldTimeoutWhenInitializingLandForIrrigation(){
+    void shouldTimeOutWhenInitializingLandForIrrigation(){
         land.setLandIndex(1);
         doThrow(new IrrigationInitException("unable to process request")).when(Mockito.spy(irrigationSensorService)).initLandForIrrigation(land);
+
+        int methodCount = Mockito.mockingDetails(Mockito.spy(irrigationSensorService)).getInvocations().size();
+        assertThat(methodCount).isGreaterThan(1);
+    }
+
+    @Test
+    @DisplayName("Should initialize land for irrigation")
+    void shouldUpdateLand(){
+        assertThatThrownBy(()->irrigationSensorService.updateIrrigationSlot(land))
+                .isInstanceOf(SensorNotAvailableException.class);
+    }
+
+    @Test
+    @DisplayName("Should timeout when updating state")
+    void shouldTimeoutWhenUpdatingState(){
+        land.setLandIndex(1);
+        doThrow(new IrrigationInitException("unable to process request")).when(Mockito.spy(irrigationSensorService)).updateIrrigationSlot(land);
 
         int methodCount = Mockito.mockingDetails(Mockito.spy(irrigationSensorService)).getInvocations().size();
         assertThat(methodCount).isGreaterThan(1);
